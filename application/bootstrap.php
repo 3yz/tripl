860173590f5debb0fdc75eabaa8e97a5dd2adfde
Joel Souza <joel.souza@gmail.com>
2013-01-28 15:06:44 -0200

    Minion module

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -131,6 +131,7 @@ Kohana::modules(array(
 	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	'minion'  => MODPATH.'minion',
 	));
 
 Cookie::$salt = md5($base_url);
